45f776377c4cfddeeb78fc32d79cf9e2f93c26b8
Raúl Nimo <rauibertnf@gmail.com>
2017-10-05 14:52:59 +0200

    Añadida funcionalidad para ver mapa de tu localización

--- www/js/maps.js
+++ www/js/maps.js
@@ -8,7 +8,17 @@ var app = {
     },
 
     dispositivoPreparado: function(){
+        navigator.geolocation.getCurrentPosition(app.pintarCoords, app.errorSolicitudLocalizacion);
         navigator.geolocation.getCurrentPosition(app.dibujaCoords, app.errorSolicitudLocalizacion);
+    },
+
+    pintarCoords: function(position){
+        var mapa = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
+
+        L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ29vdHlmZXIiLCJhIjoiY2l1MGlrb2M3MDAwMDJ6bXAxY3dlOXdkYiJ9.RBfUsuzHfLrofEyMR8IVlA', {
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            maxZoom: 18
+        }).addTo(mapa);
     },
 
     dibujaCoords: function(position){
